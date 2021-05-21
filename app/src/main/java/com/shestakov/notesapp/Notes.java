df939f6a363f09b6945db45d705cacaae276ca7f
Evgeniy Shestakov <Gennadith.bear@gmail.com>
2021-05-21 16:07:26 +0500

    init

---
+++ app/src/main/java/com/shestakov/notesapp/Notes.java
@@ -0,0 +1,13 @@
+package com.shestakov.notesapp;
+
+public class Notes {
+private  String nameNotes;
+private  String descriptionNotes;
+private  String dateNotes;
+
+    public Notes(String nameNotes, String descriptionNotes, String dateNotes) {
+        this.nameNotes = nameNotes;
+        this.descriptionNotes = descriptionNotes;
+        this.dateNotes = dateNotes;
+    }
+}
